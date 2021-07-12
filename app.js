b0eab7725dc7e5882846acb112043caed2adea23
Dennis de Guzman <84108180+d3guzman-dennis@users.noreply.github.com>
2021-07-12 15:44:30 -0400

    init

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello node!')
+//1
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+//9.1.6
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+  };
+  
+  console.log('================');
+  printProfileData(profileDataArgs);
+
+  // 9.1.6b - why return? what's the diff? why isn't it returning sum? !!!
+
+/*   const addNums = (numOne, numTwo) => {
+    console.log(numOne, numTwo);
+    return numOne + numTwo;
+  };
+
+  const sum = addNums(5, 3); */
